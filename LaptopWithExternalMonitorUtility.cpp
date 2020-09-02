769fa93522c80212c462635da02de460f39d434d
charlie0129 <55270174+charlie0129@users.noreply.github.com>
2020-09-02 16:38:01 +0800

    Add files via upload

---
+++ LaptopWithExternalMonitorUtility.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <Windows.h>
+#include <powrprof.h>
+
+BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData)
+{
+    int *Count = (int *)dwData;
+    (*Count)++;
+    return TRUE;
+}
+
+int MonitorCount()
+{
+    int Count = 0;
+
+    if (EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, (LPARAM)&Count))
+    {
+        return Count;
+    }
+
+    return -1;
+}
+
+void usage(void)
+{
+    fprintf(stderr,
+            "Usage .\\LaptopWithExternalMonitorUtility POWER_PLAN_GUID_MUTIPLE_MONITOR POWER_PLAN_GUID_SINGLE_MONITOR\n"
+            "-h  Print this help\n"
+            "Note: you need to run this utility with elevated privileges.\n"
+           );
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        usage();
+        return EXIT_FAILURE;
+    }
+
+    if (MonitorCount() < 0)
+    {
+        return EXIT_FAILURE;
+    }
+
+    std::string POWER_PLAN_GUID_MUTIPLE_MONITOR{ argv[1] };
+    std::string POWER_PLAN_GUID_SINGLE_MONITOR{ argv[2] };
+
+    // ShowWindow(GetConsoleWindow(), SW_HIDE); // hide console window
+
+    int nCurrentMonitors = 0;
+
+    while (true)
+    {
+        if (MonitorCount() == nCurrentMonitors ? false : nCurrentMonitors = MonitorCount())
+        {
+            if (nCurrentMonitors >= 2) // mutiple monitors
+            {
+
+                std::string cmd{ "power /setactive " };
+
+                if (system((cmd + POWER_PLAN_GUID_MUTIPLE_MONITOR).c_str()) != EXIT_SUCCESS)
+                {
+                    fputs("Error when setting power plans.\n", stderr);
+                    usage();
+                    return EXIT_FAILURE;
+                }
+                else
+                {
+                    puts("Activated multi-monitor power plan.\n");
+                }
+
+                // puts("mutiple");
+            }
+            else // single monitor
+            {
+                std::string cmd{ "power /setactive " };
+
+                if (system((cmd + POWER_PLAN_GUID_SINGLE_MONITOR).c_str()) != EXIT_SUCCESS)
+                {
+                    fputs("Error when setting power plans.\n", stderr);
+                    usage();
+                    return EXIT_FAILURE;
+                }
+                else
+                {
+                    puts("Activated single-monitor power plan.\n");
+                }
+
+                // puts("single");
+            }
+        }
+
+        Sleep(1000);
+    }
+
+    return EXIT_SUCCESS;
+}
